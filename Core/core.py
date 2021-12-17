1ad23f9a29ec435d28abf08435e719a225471b4f
구이타스 <rockyeo33@gmail.com>
2021-12-17 15:12:10 +0000

    코어 프레임워크 작성

--- Core/core.py
+++ Core/core.py
@@ -1,9 +1,45 @@
-from dataclasses import dataclass 
+import time
+import globalVar
+import route
 
-route_list = []
+#sentence = 출력할 문자, sleepTime = 글자가 출력되는 시간간격(단위:초)
+def printScript(sentence, sleepTime=0.1):
+	for i in range(len(sentence)):
+		if(sentence[i] == '`'):
+			input()
+			break
+		if(sentence[i] == '$'):
+			routeCheck()
+		print(sentence[i], end="")
+		time.sleep(sleepTime)
 
-@dataclass 
-class game_framework: 
-    room: int = None
-    next_room: int = None
-        
+def routeCheck():
+	while True:
+		print("당신의 선택은? >> ")
+		select = input()
+		globalVar.status.next_room = route.routeSelect(globalVar.status.room, int(select))
+		if(globalVar.status.next_room != -1):
+			print(select + " 번을 선택했습니다. \n\n")
+			globalVar.status.room = globalVar.status.next_room
+			break
+		print("잘못된 선택입니다!")
+	
+	
+def gameCore():
+	file_name = "Script/Script_" + str(globalVar.status.room) + ".txt"
+	game_script = open(file_name, "r", encoding='utf-8')
+	"""
+	#파일이 있는지 체크
+    if(game_route == None)
+    {
+        print("Route.txt가 발견되지 않았습니다! 게임을 실행할 수 없습니다!")
+        input()
+        quit()
+    }
+	"""
+	script_buffer = game_script.read()
+	
+	i = 0
+	for i in range(len(script_buffer)):
+		printScript(script_buffer[i], 0.02)
+		
